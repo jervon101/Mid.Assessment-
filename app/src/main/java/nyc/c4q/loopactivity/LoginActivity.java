b8f3edb2505405fc499516969ee411e3ea4890f7
Jervon Arnold <jervonjarnold@gmail.com>
2017-12-06 19:56:14 -0500

    added part03

--- app/src/main/java/nyc/c4q/loopactivity/LoginActivity.java
+++ app/src/main/java/nyc/c4q/loopactivity/LoginActivity.java
@@ -1,29 +1,54 @@
 package nyc.c4q.loopactivity;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String SHARED_PREF_KEY = "test";
+
+    private SharedPreferences log;
+    Button button;
+    CheckBox box;
+    EditText edit1, edit2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        log = getSharedPreferences(SHARED_PREF_KEY, MODE_PRIVATE);
+
+        button = (Button) findViewById(R.id.button);
+        box = (CheckBox) findViewById(R.id.box);
+        edit1 = (EditText) findViewById(R.id.edit01);
+        edit2 = (EditText) findViewById(R.id.edit02);
+
+        SharedPreferences.Editor editor = log.edit();
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            SharedPreferences.Editor editor = log.edit();
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                if (box.isChecked()) {
+                    editor.putString(edit1.getText().toString(), edit2.getText().toString());
+                    editor.commit();
+                    if (edit1.getText().toString().equals("user@aol.com") && edit2.getText().toString().equals("password1234")) {
+                        Intent intent = new Intent(getApplicationContext(),ListActivity.class);
+                        intent.putExtra("email",edit1.getText().toString());
+                        startActivity(intent);
+                    }
+                }
             }
         });
-    }
 
+    }
 }
